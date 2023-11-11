95eec5548bbf89672e039fbc84e0965d3396646d
Brendan McGaw <Brendanmcgaw@gmail.com>
2023-11-12 00:50:37 +1100

    Refactored  some code destablized  styles, added screens  and  move d  some components around.

--- src/screens/UpcomingWeather.js
+++ src/screens/UpcomingWeather.js
@@ -1,6 +1,13 @@
 import React from "react";
-import { SafeAreaView, View, StyleSheet, Text, FlatList, StatusBar, ImageBackground } from "react-native"
-import { Feather } from '@expo/vector-icons'
+import { 
+    SafeAreaView, 
+    StyleSheet, 
+    Text, 
+    FlatList, 
+    StatusBar, 
+    ImageBackground 
+} from "react-native"
+import ListItem from "../components/ListItem"
 
 const DATA = [
     {
@@ -41,33 +48,22 @@ const DATA = [
     }
 ]
 
-const Item = (props) => {
-    const { dt_txt, min, max, condition } = props
-    return (
-        <View style={styles.item}>
-            <Feather name={'sun'} size={50} color={'white'}/>
-            <Text style={styles.dt}>{dt_txt}</Text>
-            <Text style={styles.temp}>{min}</Text>
-            <Text style={styles.temp}>{max}</Text>
-        </View>
-    )
-}
-
 const UpcomingWeather = () => {
     const renderItem = ({ item }) => (
-        <Item  
+        <ListItem  
             condition={item.weather[0].main} 
             dt_txt={item.dt_txt} 
             max={item.main.temp_max} 
             min={item.main.temp_min} 
         />
     )
+    const { container, image } = styles
     return (
-        <SafeAreaView style={styles.container}>
+        <SafeAreaView style={container}>
             {/* Allows image to sit in the background behind all the other objects. */}
             <ImageBackground
                 source={require("../../assets/thunderstorm-3625405_1920.jpg")} 
-                style={styles.image} 
+                style={image} 
             >
                 <Text>Upcoming Weather</Text>
                 <FlatList 
@@ -85,24 +81,6 @@ const styles = StyleSheet.create({
         flex: 1,
         marginTop: StatusBar.currentHeight || 0,
         backgroundColor: "royalblue"
-    },
-    item: {
-        padding: 20,
-        marginVertical: 8,
-        marginHorizontal: 16,
-        flexDirection: "row",
-        justifyContent: "space-around",
-        alignItems: "center",
-        borderWidth: 5,
-        backgroundColor: "pink",
-    },
-    temp: {
-        color: "white",
-        fontSize: 20
-    },
-    dt: {
-        color: "white",
-        fontSize: 15
     },
     // Allows our image to fill the entire background on the page.
     image: {
