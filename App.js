9a54f744e6392c3717b0930c2b7e4261f63a97ab
Brendan McGaw <Brendanmcgaw@gmail.com>
2023-11-17 22:50:14 +1100

    Currently set to running the app if weather && weather.list exist, if not show the spinning pin wheel of death.

--- App.js
+++ App.js
@@ -5,57 +5,23 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Tabs from "./src/components/Tabs";
 import * as Location from "expo-location";
 import { WEATHER_API_KEY } from "@env";
+import { useGetWeather } from "./src/hooks/useGetWeather";
 
 const App = () => {
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-  const [weather, setWeather] = useState([]);
-  const [lat, setLat] = useState([]);
-  const [lon, setLon] = useState([]);
+  const [loading, error, weather] = useGetWeather();
+  console.log(loading, error, weather);
 
-  const fetchWeatherData = async () => {
-    try {
-      const res = await fetch(
-        `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`
-      );
-      const data = await res.json();
-      setWeather(data);
-      setLoading(false);
-    } catch (error) {
-      setError("Could not fetch weather.");
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    // While waiting for permission (Async allows task to pause and do other tasks) it will continue to load geolocation.
-    (async () => {
-      let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== "granted") {
-        setError("permission to access location was denied");
-        return;
-      }
-      let location = await Location.getCurrentPositionAsync({});
-      setLat(location.coords.latitude);
-      setLon(location.coords.longitude);
-      await fetchWeatherData();
-      console.log(weather);
-    })(); // immediate invoking on function we put () afterwards.
-  }, [lat, lon]); // Array is for the dependencies. This allows the useEffect to only run once when the component is first run.
-
-  if (loading) {
+  if (weather && weather.list) {
     return (
-      <View style={styles.loadingContainer}>
-        <ActivityIndicator size={"large"} color={"blue"} />
-      </View>
+      <NavigationContainer>
+        <Tabs weather={weather} />
+      </NavigationContainer>
     );
   }
-
   return (
-    <NavigationContainer>
-      <Tabs />
-    </NavigationContainer>
+    <View style={styles.loadingContainer}>
+      <ActivityIndicator size={"large"} color={"blue"} />
+    </View>
   );
 };
 
---
+++ src/hooks/useGetWeather.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from "react";
+import { ActivityIndicator, View, StyleSheet } from "react-native";
+import * as Location from "expo-location";
+import { WEATHER_API_KEY } from "@env";
+
+export const useGetWeather = () => {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [weather, setWeather] = useState([]);
+  const [lat, setLat] = useState([]);
+  const [lon, setLon] = useState([]);
+
+  const fetchWeatherData = async () => {
+    try {
+      const res = await fetch(
+        `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`
+      );
+      const data = await res.json();
+      setWeather(data);
+      setLoading(false);
+    } catch (error) {
+      setError("Could not fetch weather.");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    // While waiting for permission (Async allows task to pause and do other tasks) it will continue to load geolocation.
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setError("permission to access location was denied");
+        return;
+      }
+      let location = await Location.getCurrentPositionAsync({});
+      setLat(location.coords.latitude);
+      setLon(location.coords.longitude);
+      await fetchWeatherData();
+      console.log(weather);
+    })(); // immediate invoking on function we put () afterwards.
+  }, [lat, lon]); // Array is for the dependencies. This allows the useEffect to only run once when the component is first run.
+  return [loading, error, weather];
+};
