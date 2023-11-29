b10eb1ab0db464e6098f872ddb975ad56e89fe70
Brendan McGaw <Brendanmcgaw@gmail.com>
2023-11-29 22:58:35 +1100

    Some sudo code to help write the rest of the code.

--- src/screens/Monday.js
+++ src/screens/Monday.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { View, Text, StyleSheet, SafeAreaView, FlatList } from "react-native";
 import ListItem from "../components/ListItem";
+import moment from "moment";
 
 const Monday = ({ weatherData }) => {
-  console.log({ weatherData });
+  console.log(weatherData.dt_txt);
   const renderItem = ({ item }) => (
     <ListItem
       condition={item.weather[0]?.main}
@@ -13,6 +14,13 @@ const Monday = ({ weatherData }) => {
     />
   );
   const { container } = styles;
+  // if (moment(weatherData.dt_txt).format("dddd") == "Monday") {
+  const dayName = moment(weatherData[0].dt_txt).format("dddd");
+  // weatherData[0].dt_txt;
+  // const day1 = dayName.getDay();
+  console.log(dayName);
+  // length of weatherData = 40. Iterate through i++ until == 40 and check each day to find all days that are the same and then render only those days datas.
+  console.log(weatherData.length);
   return (
     <SafeAreaView style={container}>
       <FlatList
